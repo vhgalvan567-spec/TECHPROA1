155c52b64c7f127a7733dd98eeb5eece6e526a5f
VinceHarold <vhgalvan567@gmail.com>
2025-10-24 07:11:53 +0000

    Commit Seatwok Fifteen

--- SeatworkFifteen.java
+++ SeatworkFifteen.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class SeatworkFifteen {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+                    
+                            System.out.println("Enter the student score:");
+                                    int score = input.nextInt();
+                                            
+                                                    char grade;
+                                                            
+                                                                    if (score >= 90) {
+                                                                                System.out.println("The student grade is A");
+                                                                                        } else if (score >= 80) {
+                                                                                                    System.out.println("The student grade is B");
+                                                                                                            } else if (score >= 70) {
+                                                                                                                  System.out.println("The student grade is C");
+                                                                                                                          } else if (score >= 60) {
+                                                                                                                                 System.out.println("The student grade is D");
+                                                                                                                                         } else if (score <= 59) {
+                                                                                                                                           System.out.println("The student grade is F");
+                                                                                                                                                   }
+                                                                                                                                                       }
+                                                                                                                                                       }
